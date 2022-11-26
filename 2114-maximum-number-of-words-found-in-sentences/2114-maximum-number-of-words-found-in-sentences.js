6d1f187295b842336b2135b01899dd34b1b14097
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2022-11-26 12:15:29 +0530

    Time: 80 ms (81.28%), Space: 43.7 MB (85.84%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var mostWordsFound = function(sentences) {
-    let spaceSplit = sentences.map((test)=>(test.split(",")));
+    let spaceSplit = sentences.map(test=>test.split(","));
     let newSplit = spaceSplit.map(test=>test.toString());
     let newSplit2 = newSplit.map(test=>test.split(" ").length);
     return (Math.max.apply(0, newSplit2));
