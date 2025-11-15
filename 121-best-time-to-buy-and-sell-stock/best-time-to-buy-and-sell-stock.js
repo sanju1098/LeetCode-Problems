83d69b9e5b8d9bf47a9456f7bea8fab4295360a3
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2025-11-15 19:02:08 +0530

    Time: 3 ms (58.85%) | Memory: 63.9 MB (96.77%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,18 +3,12 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    if (prices.length < 2) return 0;
-
-    let minPrice = prices[0];
+    let min = prices[0];
     let maxProfit = 0;
 
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < minPrice) { // Update the minimum price if the current price is lower
-            minPrice = prices[i];
-        } else {
-            const profit = prices[i] - minPrice;
-            maxProfit = Math.max(maxProfit, profit);
-        }
+    for (i = 1; i < prices.length; i++) {
+        if ((prices[i] - min) > maxProfit) { maxProfit = prices[i] - min }
+        if (prices[i] < min) { min = prices[i] }
     }
-    return maxProfit;
+    return maxProfit
 };
