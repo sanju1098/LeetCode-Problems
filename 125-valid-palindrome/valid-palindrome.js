95a892cc916b16a4a1b4854f38d8e3bb7a91f241
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-04 20:45:03 +0530

    Time: 11 ms (14.33%) | Memory: 60 MB (31.87%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,14 +4,44 @@
  */
 var isPalindrome = function (s) {
     s = s.toLowerCase();
-    let filteredString = '';
-    let rev = '';
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i].match(/[a-z0-9]/i)) {
-            filteredString = filteredString + s[i]
-            rev = s[i] + rev
+    // Extra Space
+    /**
+    Time - O(n)
+    Space - O(n)
+     */
+    // let filteredString = '';
+    // let rev = '';
+
+    // for (let i = 0; i < s.length; i++) {
+    //     if (s[i].match(/[a-z0-9]/i)) {
+    //         filteredString = filteredString + s[i]
+    //         rev = s[i] + rev
+    //     }
+    // }
+    // return filteredString === rev;
+    // ========================================================================
+    // Optimised
+    /**
+    Two Pointer Approach
+    Time - O(n)
+    Space - O(1)
+     */
+
+    let i = 0;
+    let j = s.length - 1
+    while (i < j) {
+        if (!s[i].match(/[a-z0-9]/i)) {
+            i++
         }
+        else if (!s[j].match(/[a-z0-9]/i)) {
+            j--
+        }
+        else if (s[i] === s[j]) {
+            i++;
+            j--;
+        }
+        else return false
     }
-    return filteredString === rev;
+    return true
 };
