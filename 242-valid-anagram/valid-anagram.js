c8db8b53c8d41af5c5acf5cb12bbdf5f450df862
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-05 12:45:11 +0530

    Time: 11 ms (77.38%) | Memory: 55.1 MB (83.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,6 +4,34 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    if (s.length !== t.length) return false;
-    return s.split("").sort().join("") === t.split("").sort().join("");
+    /** Sort and Split => Time - O(n log n) || Space - O(n) */
+    // if (s.length !== t.length) return false;
+    // return s.split("").sort().join("") === t.split("").sort().join("");
+
+    /** Optimised Approach */
+    /**Hash Map Approach
+    
+    s = anagram => {a:3, n:1, g:1, r:1, m:1}
+    and now decremenet the Map with comparing t => Start Traversing of t and start reducing the count
+    t = {a:0, n:0, g:0, r:0, m:0}
+
+    if all the amp of t is ) then true else false
+     */
+
+    let map = {};
+    if (s.length != t.length) return false
+
+    for (let i = 0; i < s.length; i++) { // O(n)
+        if (!map[s[i]]) {
+            map[s[i]] = 1;
+        } else { ++map[s[i]] }
+    }
+
+    for (let i = 0; i < t.length; i++) { // O(n)
+        if (!map[t[i]] || map[t[i]] < 0) {
+            return false
+        } else { --map[t[i]] }
+    }
+    /** Time - O(n) + O(n) = O(n) || Space - O(1) */
+    return true
 };
