3afebd87826efc01fd3d7f1429e32de35db77d25
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-15 22:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (16.60%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    if (n <= 2) {
+        return n;
+    }
+
+    let first = 1;
+    let second = 2;
+
+    for (let i = 3; i <= n; i++) {
+
+        let current = first + second;
+
+        first = second;
+        second = current;
+    }
+
+    return second;
+};
