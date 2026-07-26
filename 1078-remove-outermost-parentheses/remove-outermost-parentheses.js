039d7faa353d507bbba6073e9cfbd6055979d1da
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-26 12:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 56.6 MB (49.90%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -3,22 +3,39 @@
  * @return {string}
  */
 var removeOuterParentheses = function (s) {
-    let stack = [];
+    // With Stack
+    // let stack = [];
+    // let ans = ""
+    // for (let i = 0; i < s.length; i++) {
+    //     if (s[i] === "(") {
+    //         stack.push(s[i])
+    //         let len = stack.length
+    //         if (len > 1) {
+    //             ans = ans + s[i]
+    //         }
+    //     }
+    //     else {
+    //         let len = stack.length
+    //         if (len > 1) {
+    //             ans = ans + s[i]
+    //         }
+    //         stack.pop()
+    //     }
+    // }
+    // return ans
+
+    // Without Stack
+    let level = 0;
     let ans = ""
+
     for (let i = 0; i < s.length; i++) {
         if (s[i] === "(") {
-            stack.push(s[i])
-            let len = stack.length
-            if (len > 1) {
-                ans = ans + s[i]
-            }
+            level++
+            ans += level > 1 ? s[i] : ""
         }
         else {
-            let len = stack.length
-            if (len > 1) {
-                ans = ans + s[i]
-            }
-            stack.pop()
+            ans += level > 1 ? s[i] : ""
+            level--
         }
     }
     return ans
