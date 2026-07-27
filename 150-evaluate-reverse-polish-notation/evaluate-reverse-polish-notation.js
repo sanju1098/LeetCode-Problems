5286272e9bfb391b382bf6c8bddd7e18ec47e279
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-27 11:22:31 +0530

    Time: 19 ms (19.99%) | Memory: 60 MB (19.09%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+    let stack = []
+    for (let i = 0; i < tokens.length; i++) {
+        if (tokens[i] === "+" || tokens[i] === "-" || tokens[i] === "*" || tokens[i] === "/") {
+            let a = stack.pop()
+            let b = stack.pop()
+            let ans = eval(`${b} ${tokens[i]} ${a}`)
+            stack.push(Math.trunc(ans))
+        } else { stack.push(tokens[i]) }
+    }
+    return Number(stack.pop())
+};
