0c70415af5cca8b054b91bca867abecde73f1b3c
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-30 16:14:42 +0530

    Time: 1 ms (99.92%) | Memory: 62.6 MB (54.11%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+    function mergeTwoLists(l1, l2) {
+        const dummy = new ListNode();
+        let tail = dummy;
+        while (l1 && l2) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = l1 || l2;
+        return dummy.next;
+    }
+
+    if (lists.length === 0) return null;
+    while (lists.length > 1) {
+        const merged = [];
+        for (let i = 0; i < lists.length; i += 2) {
+            const l1 = lists[i];
+            const l2 = i + 1 < lists.length ? lists[i + 1] : null;
+            merged.push(mergeTwoLists(l1, l2));
+        }
+        lists = merged;
+    }
+    return lists[0];
+};
