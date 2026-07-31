805272d6aaa32df0e243ce7ebdeb4845232b54b6
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-31 19:41:56 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (28.73%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (!root) return null;
+    const queue = [root];
+    while (queue.length) {
+        const node = queue.shift();
+        let temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return root;
+};
