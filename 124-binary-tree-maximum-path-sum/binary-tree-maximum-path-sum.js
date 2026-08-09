1518c0597d421896ff18ade5a8631972a47cd42d
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-08-09 11:30:40 +0530

    Time: 3 ms (25.43%) | Memory: 64.8 MB (59.57%) - LeetSync

---
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function (root) {
+    let maxSum = -Infinity;
+    function dfs(node) {
+        if (!node) return 0;
+        const left = Math.max(0, dfs(node.left));
+        const right = Math.max(0, dfs(node.right));
+        const pathSum = left + node.val + right;
+        maxSum = Math.max(maxSum, pathSum);
+        return node.val + Math.max(left, right);
+    }
+    dfs(root);
+    return maxSum;
+};
